f3cb5f9bbd2413b4fee48248ec2f6004141ceabc
Ashal aka JOKER <ahastudio@gmail.com>
2019-10-15 23:26:53 +0900

    프로자백러 Day 1
    프로자백러 첫 수업 코드.
    
    https://dal-lab.com/java-backend/

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# cat-toys-online-shop
-Cat Toys Online Shop
+# Cat Toys Online Shop
+
+[프로자백러](https://dal-lab.com/java-backend/) 예제 프로젝트.
---
+++ src/main/java/com/dallab/cattoy/CattoyApplication.java
@@ -0,0 +1,13 @@
+package com.dallab.cattoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CattoyApplication.class, args);
+	}
+
+}
