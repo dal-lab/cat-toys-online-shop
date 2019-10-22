16fe4a00345cd855594195c56730601f16c39476
Ashal aka JOKER <ahastudio@gmail.com>
2019-10-22 19:04:59 +0900

    GreetingController 테스트 추가
    Write a controller test with MockMvc.

---
+++ src/test/java/com/dallab/cattoy/controller/GreetingControllerTest.java
@@ -0,0 +1,29 @@
+package com.dallab.cattoy.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
+public class GreetingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello")));
+    }
+
+}
