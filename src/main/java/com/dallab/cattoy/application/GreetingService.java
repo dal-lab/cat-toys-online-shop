9999a1daa8c03fdc06e1b18e20295be3ad6b4ed5
Ashal aka JOKER <ahastudio@gmail.com>
2019-10-22 22:56:06 +0900

    Application 레이어에서 Domain 레이어 사용
    GreetingService에서 Greeting 도메인 모델을 사용하게 함.
    
    비즈니스 로직을 변경해야 할 때는 도메인 모델만 변경하면 된다.

--- src/main/java/com/dallab/cattoy/application/GreetingService.java
+++ src/main/java/com/dallab/cattoy/application/GreetingService.java
@@ -1,15 +1,17 @@
 package com.dallab.cattoy.application;
 
+import com.dallab.cattoy.domain.Greeting;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
     public String getMessage(String name) {
-        if (name == null) {
-            return "Hello";
-        }
-        return "Hello, " + name;
+        Greeting greeting = Greeting.builder()
+                .name(name)
+                .build();
+
+        return greeting.getMessage();
     }
 
 }
