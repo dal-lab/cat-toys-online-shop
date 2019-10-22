ba901e9783086c23e1506b577e0eb3bcb3f0192f
Ashal aka JOKER <ahastudio@gmail.com>
2019-10-22 19:29:20 +0900

    Mock Object 활용
    Application 레이어와 독립적으로 UI 레이어 테스트.

--- src/test/java/com/dallab/cattoy/controller/GreetingControllerTest.java
+++ src/test/java/com/dallab/cattoy/controller/GreetingControllerTest.java
@@ -1,15 +1,17 @@
 package com.dallab.cattoy.controller;
 
 import com.dallab.cattoy.application.GreetingService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,8 +23,15 @@ public class GreetingControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @SpyBean
+    @MockBean
     private GreetingService greetingService;
+
+    @Before
+    public void mockGreetingService() {
+        given(greetingService.getMessage(null)).willReturn("Hello");
+
+        given(greetingService.getMessage("JOKER")).willReturn("Hello, JOKER");
+    }
 
     @Test
     public void hello() throws Exception {
