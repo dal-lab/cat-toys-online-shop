a54dffa596aec7abe24cc5f551cd4a03c7aa95a2
Ashal aka JOKER <ahastudio@gmail.com>
2019-10-29 19:24:19 +0900

    Map Entity to DTO
    Use Dozer Bean Mapper.
    
    https://github.com/DozerMapper/dozer

--- src/main/java/com/dallab/cattoy/CattoyApplication.java
+++ src/main/java/com/dallab/cattoy/CattoyApplication.java
@@ -1,13 +1,21 @@
 package com.dallab.cattoy;
 
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CattoyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CattoyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CattoyApplication.class, args);
+    }
+
+    @Bean
+    public Mapper modelMapper() {
+        return DozerBeanMapperBuilder.buildDefault();
+    }
 
 }
--- src/main/java/com/dallab/cattoy/controller/ProductController.java
+++ src/main/java/com/dallab/cattoy/controller/ProductController.java
@@ -3,7 +3,7 @@ package com.dallab.cattoy.controller;
 import com.dallab.cattoy.application.ProductService;
 import com.dallab.cattoy.domain.Product;
 import com.dallab.cattoy.dto.ProductDto;
-import org.apache.logging.log4j.util.PropertiesUtil;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +15,18 @@ import java.util.stream.Collectors;
 public class ProductController {
 
     @Autowired
+    Mapper mapper;
+
+    @Autowired
     private ProductService productService;
 
     @GetMapping("/products")
     public List<ProductDto> list() {
         List<Product> products = productService.getProducts();
 
-        return products.stream().map(product -> {
-            ProductDto productDto = new ProductDto();
-            productDto.setName(product.getName());
-            return productDto;
-        }).collect(Collectors.toList());
+        return products.stream()
+                .map(product -> mapper.map(product, ProductDto.class))
+                .collect(Collectors.toList());
     }
+
 }
