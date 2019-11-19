ed1bff328b50d997154276e5380d0795a8578d99
Ashal aka JOKER <ahastudio@gmail.com>
2019-11-19 13:34:40 +0900

    Product list to ProductDto list

--- src/main/java/com/dallab/cattoys/controller/ProductController.java
+++ src/main/java/com/dallab/cattoys/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -15,20 +16,25 @@ public class ProductController {
 
     @GetMapping
     public List<ProductDto> list() {
-        Product product = Product.builder()
-                .name("쥐돌이")
-                .maker("달랩")
-                .price(6000)
-                .build();
-
-        List<ProductDto> productDtos = new ArrayList<>();
-        productDtos.add(
-                ProductDto.builder()
-                        .name(product.getName())
-                        .maker(product.getMaker())
-                        .price(product.getPrice())
+        List<Product> products = Arrays.asList(
+                Product.builder()
+                        .name("쥐돌이")
+                        .maker("달랩")
+                        .price(6000)
                         .build()
         );
+
+        List<ProductDto> productDtos = new ArrayList<>();
+
+        for (Product product : products) {
+            productDtos.add(
+                    ProductDto.builder()
+                            .name(product.getName())
+                            .maker(product.getMaker())
+                            .price(product.getPrice())
+                            .build()
+            );
+        }
 
         return productDtos;
     }
