2bb6d39b3a96452d11685f8c5965923ef0ff12cf
Ashal aka JOKER <ahastudio@gmail.com>
2019-11-19 16:46:59 +0900

    프론트엔드에서 상품 목록 보여주기

--- src/main/java/com/dallab/cattoys/controller/ProductController.java
+++ src/main/java/com/dallab/cattoys/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.dallab.cattoys.application.ProductService;
 import com.dallab.cattoys.domain.Product;
 import com.dallab.cattoys.dto.ProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/products")
 public class ProductController {
--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
 async function loadMessage() {
-  const url = 'http://localhost:8080/hello';
-  const { data } = await axios.get(url);
-  const { name, message } = data;
+  const url = 'http://localhost:8080/products';
+  const { data: products } = await axios.get(url);
   const element = document.getElementById('app');
   element.innerHTML = `
-    <p>이름: ${name}</p>
-    <p>메시지: ${message}</p>
+    ${products.map(product => `
+      <div>
+        [${product.maker}]
+        ${product.name}
+        /
+        ${product.price}원
+      </div>
+    `).join('')}
   `;
 }
 
