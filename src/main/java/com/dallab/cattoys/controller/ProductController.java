19899a218e0160df5d4d7b88d13cf24a70cf6479
Ashal aka JOKER <ahastudio@gmail.com>
2019-11-19 13:24:40 +0900

    JSON으로 응답

--- src/main/java/com/dallab/cattoys/controller/ProductController.java
+++ src/main/java/com/dallab/cattoys/controller/ProductController.java
@@ -1,16 +1,28 @@
 package com.dallab.cattoys.controller;
 
+import com.dallab.cattoys.domain.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
     @GetMapping
-    public String list() {
-        return "쥐돌이";
+    public List<Product> list() {
+        Product product = Product.builder()
+                .name("쥐돌이")
+                .maker("달랩")
+                .price(6000)
+                .build();
+
+        List<Product> products = Arrays.asList(product);
+
+        return products;
     }
 
 }
--- src/test/java/com/dallab/cattoys/controller/ProductControllerTest.java
+++ src/test/java/com/dallab/cattoys/controller/ProductControllerTest.java
@@ -3,8 +3,10 @@ package com.dallab.cattoys.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,9 +19,17 @@ class ProductControllerTest {
 
     @Test
     public void list() throws Exception {
-        mockMvc.perform(get("/products"))
+        mockMvc.perform(get("/products")
+                // 최신 브라우저는 UTF-8이 기본이지만 MockMvc는 아니라 따로 지정해야 함.
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+        )
                 .andExpect(status().isOk())
-                .andExpect(content().string("쥐돌이"));
+                // 인코딩이 올바른지 검사.
+                .andExpect(content().encoding("UTF-8"))
+                // JSON 데이터 검사.
+                .andExpect(content().string(
+                        containsString("\"name\":\"쥐돌이\"")
+                ));
     }
 
 }
