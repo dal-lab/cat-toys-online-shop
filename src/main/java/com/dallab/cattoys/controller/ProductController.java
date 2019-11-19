3006c88220fefbe79300a63a3fc6441635a6b1e1
Ashal aka JOKER <ahastudio@gmail.com>
2019-11-19 11:28:14 +0900

    ProductController 기본 뼈대 작성

--- src/main/java/com/dallab/cattoys/controller/ProductController.java
+++ src/main/java/com/dallab/cattoys/controller/ProductController.java
@@ -1,4 +1,16 @@
 package com.dallab.cattoys.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping
+    public void list() {
+        // TODO: 제대로 뭔가 해야 함.
+    }
+
 }
