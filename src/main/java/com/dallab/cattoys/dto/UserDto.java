f140eecb3332260a53e2fb9418cc1b16632de2d2
Ashal aka JOKER <ahastudio@gmail.com>
2019-12-03 22:34:04 +0900

    User DTO 추가

--- src/main/java/com/dallab/cattoys/dto/UserDto.java
+++ src/main/java/com/dallab/cattoys/dto/UserDto.java
@@ -1,4 +1,24 @@
 package com.dallab.cattoys.dto;
 
+import com.github.dozermapper.core.Mapping;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+
+    @Mapping("name")
+    private String name;
+
+    @Mapping("email")
+    private String email;
+
+    @Mapping("password")
+    private String password;
+
 }
